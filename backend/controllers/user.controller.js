d695b06b15de192404cc9dcbd740f999413bf01b
uday-bhatiya <udaybhatiya31@gmail.com>
2024-12-03 21:00:02 +0530

    add user login endpoint

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,35 +1,106 @@
 import { validationResult } from "express-validator"
 import { UserModel } from "../models/user.model.js";
-import {createUser} from '../services/user.service.js';
+import { createUser } from '../services/user.service.js';
 
 
 const register = async (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            errors: errors.array()
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            })
+        }
+
+        const { fullName, email, password } = req.body;
+
+        const isUserExist = await UserModel.findOne({ email });
+        if (isUserExist) {
+            return res.status(400).json({
+                success: false,
+                message: "User already exist"
+            });
+        }
+
+        const hashedPassword = await UserModel.hashPassword(password);
+
+        const user = await createUser({
+            firstName: fullName.firstName,
+            lastName: fullName.lastName,
+            email,
+            password: hashedPassword
+        })
+
+        const token = user.generateAuthToken();
+
+        return res.status(201).json({
+            success: true,
+            message: "User registed succussfully",
+            token,
+            user: {
+                id: user._id,
+                firstName: fullName.firstName,
+                lastName: fullName.lastName,
+                email: user.email,
+            },
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
         })
     }
-
-    const { fullName, email, password} = req.body;
-
-    const isUserExist = await UserModel.findOne({email});
-    if (isUserExist) {
-        return res.status(400).json({message: "User already exist"});
-    }
-
-    const hashedPassword = await UserModel.hashPassword(password);
-
-    const user = await createUser({
-        firstName: fullName.firstName,
-        lastName: fullName.lastName,
-        email,
-        password: hashedPassword
-    })
-
-    const token = user.generateAuthToken();
-
-    res.status(201).json({ token, user });
 }
 
-export {register}
+const userLogin = async (req, res, next) => {
+
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required!",
+            })
+        }
+
+        const user = await UserModel.findOne({ email }).select('+password');
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Failed to fatch user",
+            })
+        }
+
+        const isPasswordCorrect = await user.comparePassword(password);
+        if (!isPasswordCorrect) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid password"
+            })
+        }
+
+        const token = user.generateAuthToken();
+
+        res.cookie('token', token, {
+            httpOnly: true
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            user: {
+                id: user._id,
+                email: user.email,
+            },
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+}
+
+export { register, userLogin }
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { body } from 'express-validator';
-import { register } from '../controllers/user.controller.js';
+import { register, userLogin } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
@@ -9,5 +9,10 @@ router.post('/register', [
     body('fullName.firstName').isLength({min:3}).withMessage('First name must be at least 3 characters long'),
     body('password').isLength({min:8}).withMessage('Password must be at least 8 characters long')
 ], register );
+
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({min:8}).withMessage("Password must be 8 characters long")
+], userLogin)
 
 export default router;
